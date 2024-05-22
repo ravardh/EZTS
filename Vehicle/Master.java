e3be01b503ce1e7e9953546232dfacc59d5ded9c
Raj Vardhan <rajvardaan130@gmail.com>
2024-05-22 17:29:19 +0530

    abcd

---
+++ Vehicle/Master.java
@@ -0,0 +1,27 @@
+package Vehicle;
+
+public class Master {
+	private int Model;
+	private int Engine;
+	private String Color;
+	
+	public Master(int model, int engine, String color) {
+		Model = model;
+		Engine = engine;
+		Color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Master [Model=" + Model + ", Engine=" + Engine + ", Color=" + Color + "]";
+	}
+	
+	void Masterout()
+	{
+		System.out.println("Model = " + Model);
+		System.out.println("Engine = " + Engine);
+		System.out.println("Colour = " + Color);
+	}	
+	
+	
+	}
